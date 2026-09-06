e6543b873581f140a16bfcbf2e6634c465bb3fee
Nathalie Saouli <nathalie.saouli@gmail.com>
2026-09-06 19:00:19 +0200

    Add transform script to clean data and save it as a csv file

---
+++ src/transform.py
@@ -0,0 +1,50 @@
+import json
+import os
+
+import pandas as pd
+
+# opening the json file and read it as python with load
+with open("data/raw/hiv_prevalence_raw.json", "r") as f:
+    raw_data = json.load(f)
+
+# the list of dictionaries becomes an actual data frame with rows and columns
+df = pd.DataFrame(raw_data)
+
+# Drop the fully null columns
+df = df.dropna(axis=1, how="all")
+
+
+# drop the duplicate/constants columns
+df = df.drop(columns=["ParentLocationCode", "Value", "IndicatorCode"])
+
+# filter down to country-level rows only
+df = df[df["SpatialDimType"] == "COUNTRY"] # True or False for each row
+
+# drop unnecessary columns
+df = df.drop(columns=["TimeDimensionValue", "TimeDimensionBegin", "TimeDimensionEnd"])
+
+# they both have 1 value each so we can drop them
+df = df.drop(columns=["Date", "TimeDimType"])
+
+# after cleaning, I only have 1 spatial dim type which is country so i can drop it
+df = df.drop(columns=["SpatialDimType"])
+
+# rename columns to make data analysis easier
+df = df.rename(columns={
+    "SpatialDim": "country_code",
+    "ParentLocation": "region",
+    "TimeDim": "year",
+    "NumericValue": "hiv_prevalence_pct",
+    "Low": "ci_low",
+    "High": "ci_high",
+})
+
+# a duplicate check
+assert df.duplicated(subset=["country_code", "year"]).sum() == 0, "Duplicate country-year rows found!"
+
+# save data in a csv file
+# index=False, to avoid a column of internal row position number from pandas
+os.makedirs("data/processed", exist_ok=True)
+df.to_csv("data/processed/hiv_prevalence_clean.csv", index=False)
+
+print("Saved to data/processed/hiv_prevalence_clean.csv")
